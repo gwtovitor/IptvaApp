1006aac68e0ac1374483df613756acb771fc07ee
Vitor Augusto <gwtovitorpw@gmail.com>
2023-02-22 18:58:19 -0300

    Revert "update"
    This reverts commit a386bb24f3a97e44fd0e64386a24abb1c8d6c3cb.

--- src/services/postapi.js
+++ src/services/postapi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apipost = axios.create({
 
-        baseURL:'http://192.168.0.107:3333/'
+        baseURL:'http://192.168.1.43:3333/'
 
 })
 export default apipost
